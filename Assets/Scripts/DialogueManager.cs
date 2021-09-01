01838d54c1e6049ed8302c10bb44401953d25c4d
Sebastián Millán <sebasmillan02@gmail.com>
2021-09-01 18:03:01 -0500

    Base de diálogos
    - Eliminación Debug.Logs de la animación que aparecían en la consola.
    
    - Implementación de la base del sistema de diálogos por click al interactuar con un NPC
    
    -Problema leve con los colliders, pero funcional
    
    - Cambio del proyecto (Pasa de PC a Android)
    
    - Cambio del tamaño del Joystick

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Dialogue dialogue; 
+
+    Queue<string> sentences;
+
+    public GameObject dialoguePanel;
+    public TextMeshProUGUI displayText;
+
+    string activeSentence;
+
+    public float typingSpeed;
+    AudioSource Myaudio;
+    public AudioClip speakSound;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+        Myaudio = GetComponent<AudioSource>();
+    }
+
+    void StartDialogue()
+    {
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentenceList)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    void DisplayNextSentence()
+    {
+        if (sentences.Count <= 0)
+        {
+            displayText.text = activeSentence;
+            return; 
+        }
+
+        activeSentence = sentences.Dequeue();
+        Debug.Log(activeSentence);
+    }
+
+    private void OnTriggerEnter2D(Collider2D CircleColl)
+    {
+        if (CircleColl.CompareTag("Player"))
+        {
+            StartDialogue();
+        }
+
+    }
+
+    void OnTriggerStay2D(Collider2D StayColl)
+    {
+        if (StayColl.CompareTag("Player"))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                DisplayNextSentence();
+            }
+        }
+    }
+}
